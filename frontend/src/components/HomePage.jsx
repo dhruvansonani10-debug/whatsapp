857520afc166bd69b48f564d3199d7e5969c3730
Sonani Dhruvan <dhruvansonani10@gmail.com>
2026-06-23 21:00:52 +0530

    sidbar

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,10 +1,40 @@
 import React from "react";
+import Layout from "./Layout";
+import ChatList from "../pages/chatSection/ChatList";
+import useLayoutStore from "../../store/layoutStore";
+import { motion } from "framer-motion";
+import { getAllUsers } from "../../api/message";
+import { useState,useEffect } from "react";
 
 function HomePage() {
+  const setSelectedContact = useLayoutStore(state => state.setSelectedContact);
+  const [allUsers,setAllUsers] = useState([]);
+  const getAllUser = async()=>{
+    try {
+      const result = await getAllUsers();
+      if(result.success)
+      {
+        setAllUsers(result.data);
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    getAllUser();
+  }, [])
+  
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Layout>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        className="h-full"
+      >
+        <ChatList contacts={allUsers} setSelectedContact={setSelectedContact}/>
+      </motion.div>
+    </Layout>
   );
 }
 
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useLayoutEffect } from "react";
+import useThemeStore from "../../store/themeStore";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import useUserStore from "../../store/useUserStore";
+import useLayoutStore from "../../store/layoutStore";
+import { FaWhatsapp,FaUser,FaCog,FaRegListAlt,FaPlus,FaBars } from "react-icons/fa";
+import {MdRadioButtonChecked} from "react-icons/md";
 
 function Sidebar() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const { theme, setTheme } = useThemeStore();
+  const { user } = useUserStore();
+  const { activeTab, setActiveTab, selectedContact } = useLayoutStore();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setActiveTab("chats");
+    } else if (location.pathname === "/status") {
+      setActiveTab("status");
+    } else if (location.pathname === "/user-profile") {
+      setActiveTab("profile");
+    } else if (location.pathname === "/setting") {
+      setActiveTab("setting");
+    }
+  }, [location, setActiveTab]);
+
+  if (isMobile && selectedContact) {
+    return null;
+  }
+
+  const SidebarContent = (
+    <>
+      <Link
+        to="/"
+        className={`${isMobile ? "" : "mb-8"} ${activeTab === "chats" && "bg-gray-300 shadow-sm p-2 rounded-full"} focus:outline-none `}
+      >
+        <FaWhatsapp
+          className={`h-6 w-6 ${activeTab === "chats" ? (theme === "dark" ? "text-gray-800" : "") : theme === "dark" ? "text-gray-300" : "text-gray-800"}`}
+        />
+      </Link>
+
+      <Link
+        to="/status"
+        className={`${isMobile ? "" : "mb-8"} 
+          ${activeTab === "status" && "bg-gray-300 shadow-sm p-2 rounded-full"} 
+          focus:outline-none `}
+      >
+        <MdRadioButtonChecked
+          className={`h-6 w-6 ${activeTab === "status" ? (theme === "dark" ? "text-gray-800" : "") : theme === "dark" ? "text-gray-300" : "text-gray-800"}`}
+        />
+      </Link>
+    </>
+  );
+
   return (
-    <div>Sidebar</div>
-  )
+    <MotionConfig.div
+      intial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3 }}
+      className={`${isMobile ? "fixed bottom-0 left-0 right-0 h-16" : "w-16 h-screen borded-r-2"} 
+        ${theme === "dark" ? "bg-gray-800 border-gray-600" : "bg-[rgb(239,242,254)] border-gray-300"} bg-opacity-90 flex item-center py-4 shadow-lg 
+        ${isMobile ? "flex-row justify-around" : "flex-col justfiy-between"}`}
+    >
+      {SidebarContent}
+    </MotionConfig.div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
